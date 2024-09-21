a34c3b3fd8f03b03f308c5f28b660e17727611a8
Hau Vo <hau.vophuoc@gmail.com>
2024-09-21 21:07:55 +0700

    Be able to search garage now

--- mobile/components/dialogs/garare-picker-dialog.tsx
+++ mobile/components/dialogs/garare-picker-dialog.tsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+'use client'
+
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { View, Image, Pressable, ScrollView } from 'react-native';
 import { Input } from '../ui/input';
@@ -7,6 +8,9 @@ import { Separator } from '../ui/separator';
 import { Label } from '../ui/label';
 import { Text } from '../ui/text';
 import { Button } from '../ui/button';
+import { useRef, useState } from 'react';
+import { GoogleApi } from '@/lib/google-api';
+import { PlaceAutocompleteResult } from '@googlemaps/google-maps-services-js';
 
 export type GaragePickerDialogProps = {
     open?: boolean,
@@ -17,6 +21,9 @@ export type GaragePickerDialogProps = {
 const GaragePickerDialog = (props: GaragePickerDialogProps) => {
   const { open, onRequestClose, onChange } = props
   const insets = useSafeAreaInsets();
+  const [keyword, setKeyword] = useState<string>('')
+  const [places, setPlaces] = useState<PlaceAutocompleteResult[]>([])
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null)
   
   if (!open) {
     return null
@@ -31,42 +38,54 @@ const GaragePickerDialog = (props: GaragePickerDialogProps) => {
     }
   }
 
+  function updateKeyword(text: string) {
+    setKeyword(text)
+    startSearch(text)
+  }
+
+  function startSearch(text: string) {
+    if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current)
+    }
+
+    timeoutRef.current = setTimeout(async () => {
+        try {
+            const predictions = await GoogleApi.searchGarages(text)
+            console.log(JSON.stringify(predictions))
+            if (predictions) {
+                setPlaces(predictions)
+            }
+        } catch (err) {
+            console.log(err)
+            setPlaces([])
+        }
+    }, 1000)
+  }
+
   return (
     <Portal name='garage-input-model'>
         <View className='absolute top-0 left-0 bottom-0 right-0 bg-background flex flex-col' style={{ paddingTop: insets.top }}>
             <View className='px-4 py-4'>
-                <Input />
+                <Input value={keyword} onChangeText={updateKeyword} />
             </View>
             <Separator />
             
             <ScrollView>
                 <View className='flex flex-col'>
-                    <Pressable onPress={() => chooseGarage(true)}>
-                        <View className='flex flex-row px-4 py-2.5 gap-4'>
-                            <Image source={{ uri: 'https://lh5.googleusercontent.com/p/AF1QipPxIOaHYtWnE0nfQ6DM75PVVpAt2uXP8y6-gvKO=w408-h544-k-no' }} className='mt-1.5 aspect-square w-12 h-12 rounded' />
-                            <View className='flex-1 flex flex-col'>
-                                <Label nativeID="GarageName">Euro Car Repair Solutions</Label>
-                                <Text className='text-sm text-muted-foreground'>1 Số 12, Khu phố 5, Thủ Đức, Hồ Chí Minh, Vietnam</Text>
+                    {places.map((place, index) =>
+                        <Pressable key={`${place.place_id}${index}`} onPress={() => chooseGarage(true)}>
+                            <View className='flex flex-row px-4 py-2.5 gap-4'>
+                                <Image source={{ uri: 'https://lh5.googleusercontent.com/p/AF1QipPxIOaHYtWnE0nfQ6DM75PVVpAt2uXP8y6-gvKO=w408-h544-k-no' }} className='mt-1.5 aspect-square w-12 h-12 rounded' />
+                                <View className='flex-1 flex flex-col'>
+                                    <Label nativeID="GarageName">{place.structured_formatting?.main_text || place.description}</Label>
+                                    <Text className='text-sm text-muted-foreground'>{place.description}</Text>
+                                </View>
+                                <View className='mt-0.5'>
+                                    <Text className='text-sm text-muted-foreground'>10.3km</Text>
+                                </View>
                             </View>
-                            <View className='mt-0.5'>
-                                <Text className='text-sm text-muted-foreground'>10.3km</Text>
-                            </View>
-                        </View>
-                        <Separator />
-                    </Pressable>
-                    <View>
-                        <View className='flex flex-row px-4 py-2.5 gap-4'>
-                            <Image source={{ uri: 'https://lh5.googleusercontent.com/p/AF1QipPxIOaHYtWnE0nfQ6DM75PVVpAt2uXP8y6-gvKO=w408-h544-k-no' }} className='mt-1.5 aspect-square w-12 h-12 rounded' />
-                            <View className='flex-1 flex flex-col'>
-                                <Label nativeID="GarageName">Euro Car Repair Solutions</Label>
-                                <Text className='text-sm text-muted-foreground'>1 Số 12, Khu phố 5, Thủ Đức, Hồ Chí Minh, Vietnam</Text>
-                            </View>
-                            <View className='mt-0.5'>
-                                <Text className='text-sm text-muted-foreground'>10.3km</Text>
-                            </View>
-                        </View>
-                        <Separator />
-                    </View>
+                            <Separator />
+                        </Pressable>)}
                 </View>
             </ScrollView>
 
--- mobile/lib/google-api.ts
+++ mobile/lib/google-api.ts
@@ -1,7 +1,30 @@
+import {Client, Language, LatLng, PlaceAutocompleteResult, PlaceAutocompleteType} from "@googlemaps/google-maps-services-js";
+import axios from "axios";
+
 export namespace GoogleApi {
-    export const searchPlaces = (name: string) => {
-        return [
-            
-        ]
+    const axiosInstance = axios.create({
+        url: 'https://maps.googleapis.com/maps/api'
+    })
+    const client = new Client({ axiosInstance });
+
+    // car-repair
+    export const searchGarages = async (
+        keyword: string,
+        minLength = 2,
+        language = Language.vi
+    ): Promise<PlaceAutocompleteResult[]> => {
+        if (keyword.length >= minLength) {
+            return client.placeAutocomplete({
+                params: {
+                    input: keyword,
+                    key: process.env.EXPO_PUBLIC_GOOGLE_API_KEY!,
+                    types: 'car_repair' as PlaceAutocompleteType,
+                    language,
+                    components: ['country:vn'],
+                }
+            }).then(res => res?.data?.predictions || [])
+        }
+
+        return Promise.resolve([])
     }
 }
