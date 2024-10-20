b2490992fa709eddcbcc0cbf001b81565835d9c0
Hau Vo <hau.vophuoc@gmail.com>
2024-10-20 21:10:25 +0700

    Added brand crawler

---
+++ devops/scripts/bonbanh-crawler/lib/timer.ts
@@ -0,0 +1,9 @@
+export namespace Timer {
+    export const wait = (second: number) => {
+        return new Promise<void>((resolve) => {
+            setTimeout(() => {
+                resolve()
+            }, second * 1000)
+        })
+    }
+}
