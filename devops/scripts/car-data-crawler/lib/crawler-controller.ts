55f09b12fe7456d2b69de47de1b5fb5d0ed28420
Hau Vo <hau.vophuoc@gmail.com>
2024-10-23 08:34:36 +0700

    Updated

--- devops/scripts/car-data-crawler/lib/crawler-controller.ts
+++ devops/scripts/car-data-crawler/lib/crawler-controller.ts
@@ -2,13 +2,6 @@ import { Browser, Page } from "playwright-core"
 import UserAgent from 'user-agents';
 
 export namespace CrawlerController {
-    const userAgentStrings = [
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
-    ]
-
     export const getNewPage = async (browser: Browser): Promise<Page> => {
         const userAgent = new UserAgent();
         return browser.newPage({ userAgent: userAgent.toString()})
