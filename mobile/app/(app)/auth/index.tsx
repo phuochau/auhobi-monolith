ba7429431edf264f0a23473d1c9171835bcea366
Hau Vo <hau.vophuoc@gmail.com>
2024-10-26 21:00:39 +0700

    Complete home screen

--- mobile/app/(app)/auth/index.tsx
+++ mobile/app/(app)/auth/index.tsx
@@ -2,21 +2,37 @@ import { Text } from '@/components/ui/text'
 import { Stack, useRouter } from "expo-router"
 import { Button } from "@/components/ui/button"
 import { SafeAreaView } from "react-native-safe-area-context"
+import { ImageBackground, View, Image } from 'react-native'
 
 const AuthScreen = () => {
-    const router = useRouter()
-    return (
-        <SafeAreaView>
-          <Stack.Screen options={{ headerShown: false }} />
-          <Text>Auth Screen</Text>
-          <Button onPress={() => router.push({ pathname: '/auth/login' })}>
+  const router = useRouter()
+  return (
+    <SafeAreaView>
+      <Stack.Screen options={{ headerShown: false }} />
+      <ImageBackground source={require('@/assets/images/home/bg.jpg')} resizeMode="cover" className='w-full h-full flex flex-col p-6'>
+        <View className="flex-1 flex flex-col gap-y-2 pt-12">
+          <Image source={require('@/assets/images/branding/logo.png')} resizeMode='contain' className='w-16 h-16 mb-6' />
+          <Text className="text-3xl font-semibold text-primary">Automobile, Hobbyists, Home</Text>
+          <Text className="text-accent text-lg">The go-to app for car enthusiasts! Connect, share builds, join clubs, and fuel your passion for all things automotive.</Text>
+        </View>
+        <View className="flex-1 flex flex-col justify-end gap-4 pb-4">
+          <Button
+            size={'lg'}
+            className="text-lg bg-primary"
+            onPress={() => router.push({ pathname: '/auth/login' })}
+          >
             <Text>Login</Text>
           </Button>
-          <Button onPress={() => router.push({ pathname: '/auth/register' })}>
+          <Button
+            variant={'outline'}
+            onPress={() => router.push({ pathname: '/auth/register' })}
+          >
             <Text>Register</Text>
           </Button>
-        </SafeAreaView>
-    )
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+  )
 }
 
 export default AuthScreen
--- mobile/components/ui/button.tsx
+++ mobile/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         default: 'bg-primary web:hover:opacity-90 active:opacity-90',
         destructive: 'bg-destructive web:hover:opacity-90 active:opacity-90',
         outline:
-          'border border-input bg-background web:hover:bg-accent web:hover:text-accent-foreground active:bg-accent',
+          'border border-primary bg-transparent web:hover:bg-accent web:hover:text-accent-foreground active:bg-accent',
         secondary: 'bg-secondary web:hover:opacity-80 active:opacity-80',
         ghost: 'web:hover:bg-accent web:hover:text-accent-foreground active:bg-accent',
         link: 'web:underline-offset-4 web:hover:underline web:focus:underline ',
@@ -33,13 +33,13 @@ const buttonVariants = cva(
 );
 
 const buttonTextVariants = cva(
-  'web:whitespace-nowrap text-sm native:text-base font-medium text-foreground web:transition-colors',
+  'web:whitespace-nowrap text-sm native:text-base font-semibold text-foreground web:transition-colors',
   {
     variants: {
       variant: {
         default: 'text-primary-foreground',
         destructive: 'text-destructive-foreground',
-        outline: 'group-active:text-accent-foreground',
+        outline: 'group-active:text-accent-foreground text-primary',
         secondary: 'text-secondary-foreground group-active:text-secondary-foreground',
         ghost: 'group-active:text-accent-foreground',
         link: 'text-primary group-active:underline',
