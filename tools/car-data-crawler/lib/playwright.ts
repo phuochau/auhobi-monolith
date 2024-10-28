c09043c9ac689f542d58f588df0e97e0458ced35
Hau Vo <hau.vophuoc@gmail.com>
2024-10-28 09:49:08 +0700

    Updated

--- tools/car-data-crawler/lib/playwright.ts
+++ tools/car-data-crawler/lib/playwright.ts
@@ -1,7 +1,7 @@
 import { Page } from "playwright-core";
 import { Timer } from "./timer";
 
-export const goto = async (page: Page, url: string, delay = 1.5) => {
+export const goto = async (page: Page, url: string, delay = 1) => {
     await Timer.wait(delay)
     return page.goto(url, { timeout: 120 * 1000})
 }
