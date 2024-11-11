5097a225bf53e9a0bcfe17feddff55b5238d8216
Hau Vo <hau.vophuoc@gmail.com>
2024-11-11 11:33:18 +0700

    Updated date picker

--- mobile/components/form-fields/date-time-input.tsx
+++ mobile/components/form-fields/date-time-input.tsx
@@ -1,12 +1,14 @@
 import React, { forwardRef, useState } from 'react';
-import { TextInput, View } from 'react-native';
+import { TextInput, Touchable, TouchableOpacity, View } from 'react-native';
 import { cn } from '@/lib/utils';
 import dayjs from 'dayjs'
 import { Calendar } from '@/lib/icons/Calendar'
 import { Clock8 } from '@/lib/icons/Clock8'
-import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogDescription } from '../ui/dialog';
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogDescription, DialogFooter, DialogClose } from '../ui/dialog';
 import DateTimePicker from 'react-native-ui-datepicker';
-
+import { Button } from '../ui/button';
+import { Text } from '../ui/text'
+ 
 
 type DateTimeInputProps = {
     containerClassName?: string,
@@ -59,40 +61,49 @@ const DateTimeInput = forwardRef<
     return (
         <Dialog open={shownPicker}>
             <DialogTrigger asChild>
-                <View className={cn('flex flex-col justify-center relative', containerClassName)}>
-                    <TextInput
-                        ref={ref}
-                        onPress={() => {
-                            if (!shownPicker) {
-                                setShowPicker(true)
-                            }
-                        }}
-                        className={cn(
-                            'web:flex h-10 native:h-12 web:w-full rounded-md border border-input bg-background pl-3 pr-6 web:py-2 text-base lg:text-sm native:text-lg native:leading-[1.25] text-foreground placeholder:text-muted-foreground web:ring-offset-background file:border-0 file:bg-transparent file:font-medium web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2',
-                            editable === false && 'web:cursor-not-allowed',
-                            className
-                        )}
-                        placeholderClassName={cn('text-muted-foreground', placeholderClassName)}
-                        value={displayText}
-                        {...props}
-                        editable={false}
-                    />
+                <TouchableOpacity 
+                    activeOpacity={1}
+                    className={cn('flex flex-col justify-center relative', containerClassName)}
+                    onPress={() => {
+                        if (!shownPicker) {
+                            setShowPicker(true)
+                        }
+                    }}
+                >
+                    <View pointerEvents='none'>
+                        <TextInput
+                            ref={ref}
+                            className={cn(
+                                'web:flex h-12 native:h-14 web:w-full rounded-xl border border-input bg-background pl-3 pr-6 web:py-2 text-base lg:text-sm native:text-lg native:leading-[1.25] text-foreground placeholder:text-muted-foreground web:ring-offset-background file:border-0 file:bg-transparent file:font-medium web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2',
+                                editable === false && 'web:cursor-not-allowed',
+                                className
+                            )}
+                            placeholderClassName={cn('text-muted-foreground', placeholderClassName)}
+                            value={displayText}
+                            {...props}
+                            editable={false}
+                        />
+
+                    </View>
 
                     <View className="absolute right-2.5 top-2.5">
                         {timePicker ? <Clock8 className='text-primary' /> : <Calendar className='text-primary' />}
                     </View>
-                </View>
+                </TouchableOpacity>
             </DialogTrigger>
-            <DialogContent className='sm:max-w-[425px]'>
-                <DialogHeader>
-                    <DialogDescription>
-                        <DateTimePicker
-                            mode={mode}
-                            date={date}
-                            onChange={(params: any) => onSelectDate(params)}
-                        />
-                    </DialogDescription>
-                </DialogHeader>
+            <DialogContent className='sm:max-w-[425px]' xClassName='hidden'>
+                <DateTimePicker
+                    mode={mode}
+                    date={date}
+                    onChange={(params: any) => onSelectDate(params)}
+                />
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button variant={'outline'} onPress={() => setShowPicker(false)}>
+                      <Text>Close</Text>
+                    </Button>
+                  </DialogClose>
+                </DialogFooter>
             </DialogContent>
         </Dialog>
     );
