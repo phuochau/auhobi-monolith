8dac4eefe4209a20ee11d25d9f2ccd61da2bc525
Hau Vo <hau.vophuoc@gmail.com>
2025-01-13 11:22:19 +0700

    Enable mode all in zod form

--- mobile/app/(app)/auth/forgot-password/index.tsx
+++ mobile/app/(app)/auth/forgot-password/index.tsx
@@ -34,6 +34,7 @@ const LoginScreen = () => {
     handleSubmit,
     formState: { errors, isValid },
   } = useForm<z.infer<typeof formSchema>>({
+    mode: 'all',
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: ''
