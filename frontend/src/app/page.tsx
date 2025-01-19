eb6c9a1ff17a9af99f51cff6550b7a1d9902e95e
Hau Vo <hau.vophuoc@gmail.com>
2025-01-19 20:51:38 +0700

    ads

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,7 @@
-import { Coming } from "@/components/home/coming";
-import { Navbar } from "@/components/home/navbar";
-
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col">
-      <Navbar />
-      <Coming />
+      
     </main>
   );
 }
