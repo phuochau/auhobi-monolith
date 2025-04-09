95e3a8be535dd7babb787e098a169124573da74f
Hau Vo <hau.vophuoc@gmail.com>
2025-04-09 20:40:36 +0700

    Added bare mobile react native project

---
+++ mobile-expo/metro.config.js
@@ -0,0 +1,9 @@
+const { withNativeWind } = require('nativewind/metro');
+const {
+  getSentryExpoConfig
+} = require("@sentry/react-native/metro");
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getSentryExpoConfig(__dirname);
+
+module.exports = withNativeWind(config, { input: './global.css' })
--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,9 +1,11 @@
-const { withNativeWind } = require('nativewind/metro');
-const {
-  getSentryExpoConfig
-} = require("@sentry/react-native/metro");
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getSentryExpoConfig(__dirname);
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('@react-native/metro-config').MetroConfig}
+ */
+const config = {};
 
-module.exports = withNativeWind(config, { input: './global.css' })
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
