3b3eaa3dfde572f62989ee839533ae53f89566d9
Hau Vo <hau.vophuoc@gmail.com>
2025-04-13 15:00:51 +0700

    Fixed supabasejs, react-native-config, react-native-vector-icons on mac m4 pro

--- mobile/src/config/environment.ts
+++ mobile/src/config/environment.ts
@@ -1,5 +1,6 @@
 import RNConfig from 'react-native-config';
 
 export const environment = {
-    ...RNConfig
+    SUPABASE_URL: RNConfig.SUPABASE_URL,
+    SUPABASE_ANON_KEY: RNConfig.SUPABASE_ANON_KEY
 }
